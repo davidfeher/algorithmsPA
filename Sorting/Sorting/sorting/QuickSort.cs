cadadefaf5de2a1e5cb092ae7876a25ba9c12be9
JacketWhite <feherkeee@gmail.com>
2017-04-26 00:07:32 +0200

    quicksort work in progress, needed to switch comp

---
+++ Sorting/Sorting/sorting/QuickSort.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.sorting
+{
+    class QuickSort
+    {
+        public static List<int> quickSort(List<int> rawList)
+        {
+            for (int bubble = 0; i < rawList.Count-
+            {
+
+            }
+        }
+    }
+}
