f50b1646b95eb7c4cf7061bbe1c4e4fdb7d67ae0
JacketWhite <feherkeee@gmail.com>
2017-04-26 00:09:51 +0200

    visual studio didnt save

--- Sorting/Sorting/sorting/QuickSort.cs
+++ Sorting/Sorting/sorting/QuickSort.cs
@@ -10,10 +10,15 @@ namespace Algorithms.sorting
     {
         public static List<int> quickSort(List<int> rawList)
         {
-            for (int bubble = 0; i < rawList.Count-
+            for (int bubble = 0; bubble < rawList.Count-1; bubble++) 
             {
-
+                for (int otherNumber=0; otherNumber < rawList.Count-1; otherNumber++)
+                {
+                    if (rawList[bubble] > rawList[otherNumber]) 
+                }
             }
         }
+
+        private static Swap(int index, int indexToChange)
     }
 }
