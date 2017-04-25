1114e487af7a69d42832bfc945d356de35c1a458
JacketWhite <feherkeee@gmail.com>
2017-04-25 22:54:15 +0200

    main and implemented binarysearch

---
+++ Sorting/Sorting/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int amountOfNumbers = 100;
+            Random rand = new Random();
+            List<int> myList = new List<int>();
+
+            for (int i = 0; i < amountOfNumbers; i++)
+            {
+                myList.Add(rand.Next(1, 100));
+            }
+
+            myList.Sort();
+
+            foreach(int i in myList)
+            {
+                Console.WriteLine(i);
+            }
+
+            binary.BinarySearch search = new binary.BinarySearch();
+            Console.WriteLine(search.Search(myList, 70));
+            
+        }
+    }
+}
---
+++ Sorting/Sorting/binary/BinarySearch.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.binary
+{
+    class BinarySearch
+    {
+
+        public int Search(List<int> sortedList, int numberNeeded) 
+        {
+            int min = 0;
+            int max = sortedList.Count - 1;
+            
+            while (min <= max)
+            {
+                int average = (min + max) / 2; ;
+                int guess = sortedList[average];
+
+                if (guess == numberNeeded)
+                {
+                    Console.WriteLine("We have found your number at index {0}", average);
+                    return guess;
+                }
+
+                if (guess < numberNeeded)
+                {
+                    min = average + 1;
+                }
+
+                else 
+                {
+                    max = average - 1;
+                }
+            }
+
+            return -1;
+            
+        }
+        
+    }
+}
