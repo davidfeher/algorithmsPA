343bb5ae303b2fc1cf320501ca7e5afdfe9fe978
JacketWhite <feherkeee@gmail.com>
2017-04-26 09:31:37 +0200

    implemented BubbleSort

---
+++ Sorting/Sorting/sorting/BubbleSort.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.sorting
+{
+    class BubbleSort
+    {
+        public static List<int> quickSort(List<int> unsortedList)
+        {
+           for (int i = 0; i < unsortedList.Count; i++)
+            {
+                for (int j = 1; j < (unsortedList.Count-i); j++)
+                {
+                    int temp;
+                    if (unsortedList[j-1] > unsortedList[j])
+                    {
+                        temp = unsortedList[j-1];
+                        unsortedList[j-1] = unsortedList[j];
+                        unsortedList[j] = temp;
+                    }
+                }
+            }
+            return unsortedList;
+        }
+    }
+}
